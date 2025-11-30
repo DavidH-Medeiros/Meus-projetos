a773aab1122634a2aac6c6c8a56633920aaa709c
David Henrique <davidh.medeiros2006@gmail.com>
2025-11-30 06:11:40 -0300

    Update script.js

--- Jogo-de-adivinha/Jogo_de_adivinha-main/script.js
+++ Jogo-de-adivinha/Jogo_de_adivinha-main/script.js
@@ -4,10 +4,10 @@ var tentativa = 0 ;
 var contador = 0 ;
 
 while (tentativa !== Number ) {
-    tentativa = parseInt(prompt("Por favor, insira um número inteiro de 1 a 100:"));
+    tentativa = prompt("Por favor, insira um número inteiro de 1 a 100:"+ Number );
     contador ++;
-    if (tentativa === Number) {
-        alert("Você acerto com "+ contador + " tentativas, parabens !!!!")
+    if (tentativa == Number) {
+        alert("Você acertou com "+ contador + " tentativas, parabens !!!!")
         break
     }else if (contador > 10) {
         alert("você passou de 10 tentativas")
